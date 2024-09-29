0553e07c03f78f4797e84fd0c60ab80f99899129
kirstinlyon <kirstin@mltwelve.com>
2024-09-29 14:10:34 +0200

    Bringing code in line with QFR

--- Scripts/create_budget_mfr.R
+++ Scripts/create_budget_mfr.R
@@ -12,6 +12,9 @@ OBLG_ACC_LINES_PATH <- "Data/obligation_acc_lines/"
 TRANSACTION_PATH <- "Data/transaction/"
 OUTPUT_PATH <- "Dataout/"
 
+
+#READ AND CLEAN ALL DATA ---------------------------------------------
+
 #READ IN ALL DATA---------------------------------------------------------------------
 
 #1. Active Awards (maintained by team as a google sheet but downloaded each month)----
@@ -22,7 +25,8 @@ active_awards_input_file <- dir(ACTIVE_AWARDS_FOLDER_PATH,
 active_awards_df <- map(active_awards_input_file, ~blingr::clean_awards(.x, "Active Awards")) |> 
     bind_rows() |> 
     filter(!str_detect(activity_name, paste(REMOVE_AWARDS, collapse = "|"))) |> 
-    mutate(period = lubridate::ym(period)) #convert to date
+    mutate(period = lubridate::ym(period)) |>  #convert to date 
+    mutate(fiscal_year_active_awards = lubridate::year(period))  # moved back
 
 
 write_csv(active_awards_df, paste0(OUTPUT_PATH, "active_awards.csv"))
@@ -56,7 +60,7 @@ phoenix_obl_acc_lines_df <- map(obl_acc_lines_input_file, ~ blingr::clean_phoeni
                                                                                                  active_awards_number, 
                                                                                                  OBLIGATION_TYPE_FILTER, 
                                                                                                  DISTRIBUTION_FILTER)) |> 
-    bind_rows()
+    bind_rows() 
 
 
 write_csv(phoenix_obl_acc_lines_df, paste0(OUTPUT_PATH, "phoenix_obl_acc_lines.csv"))
@@ -68,43 +72,39 @@ transaction_input_file <- dir(TRANSACTION_PATH,
 
 phoenix_transaction_df <- map(transaction_input_file, ~ blingr::clean_phoenix_transaction(.x, 
                                                                                           active_awards_number,
-                                                                                          DISTRIBUTION_FILTER, 
-                                                                                          "month")
+                                                                                          DISTRIBUTION_FILTER)
 ) |> 
     bind_rows() |> 
-    distinct() |>   #remove any duplicate rows
+    select(-program_area_name) |> 
+    rename("period_fy" = period)
+
+
+phoenix_transaction_cumulative_df <- phoenix_transaction_df |> 
     blingr::create_phoenix_transaction_cumulative()
+
+
+#
 
 write_csv(phoenix_transaction_df, paste0(OUTPUT_PATH, "phoenix_transaction.csv"))
 
-#-------------do this in Tableau not here
-# CREATE DATASETS -----------------------------------------------------------------------------------
 
-# 1. Obligation Accounting Lines
-
-active_awards_one_row <- active_awards_df |> 
+# CREATE DATASETS-----------------------------
+#1. Pipeline ----------------------
+pipeline_dataset <- active_awards_df |> 
     left_join(phoenix_obl_acc_lines_df, by = c("award_number", "period")) |> 
-    left_join(subobligation_summary_df, by = c("award_number", "period", "program_area"))|> 
-    left_join(phoenix_transaction_df, by = c("award_number",  "period" = "transaction_date" , "program_area")) 
+    left_join(subobligation_summary_df, by = c("award_number", "period", "program_area")) |> 
+    left_join(phoenix_transaction_df, by = c("award_number", "period" = "transaction_date_month", "program_area")) |> 
+    left_join(phoenix_transaction_cumulative_df, by = c("award_number", "fiscal_year_active_awards" = "fiscal_year", "program_area"))
 
-colnames(phoenix_transaction_df)
+write_csv(pipeline_dataset,"Dataout/pipeline.csv")
 
-
-
-# 2. Transaction NB:  no accruals (main difference from QFR) ----
-active_awards_one_row_transaction <- active_awards_df |> 
+#2. Transaction -------------------
+transaction_dataset <- active_awards_df |> 
     select(award_number, activity_name) |> 
-    distinct() |> #needed as there are multiple lines due to period
     left_join(phoenix_transaction_df, by = "award_number") |> 
-    select(award_number, activity_name, transaction_disbursement,
-           transaction_obligation, transaction_amt, avg_monthly_exp_rate, 
-           period, program_area, transaction_date, program_area_name, cumulative_transaction_disbursement_fy) 
+    left_join(phoenix_transaction_cumulative_df, by = c("award_number", "program_area","fiscal_year"))
 
 
-# OUTPUT DATASETS---------------------------------------------------------------
-
-write_csv(active_awards_one_row_transaction, paste0(OUTPUT_PATH, "active_awards_one_row_transaction.csv"))
-
-
+write_csv(transaction_dataset, "Dataout/transaction.csv")
 
 
